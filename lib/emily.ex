d31641a768ca089d62cb9da1a6641e56f0e5b7a3
amy null <null@amy.gg>
2018-02-23 07:57:22 -0500

    I forgot the strings ;-;

--- lib/emily.ex
+++ lib/emily.ex
@@ -15,19 +15,19 @@ defmodule Emily do
   # |> title("Test embed")
   # |> desc("This is a test of the emergency embed system")
   # |> color(0xFFFFFF)
-  # |> Emily.create_message(channel_id
+  # |> Emily.create_message(channel_id)
 
-  def create_message(content, channel_id) when is_binary(content) and is_integer(channel_id) do
+  def create_message(content, channel_id) when is_binary(content) and (is_integer(channel_id) or is_binary(channel_id)) do
     request(:post, Util.channel_messages(channel_id), %{content: content, tts: false})
     |> handle
   end
 
-  def create_message(embed, channel_id) when is_map(embed) and is_integer(channel_id) do
+  def create_message(embed, channel_id) when is_map(embed) and (is_integer(channel_id) or is_binary(channel_id)) do
     request(:post, Util.channel_messages(channel_id), %{content: "", embed: embed, tts: false})
     |> handle
   end
 
-  def create_message({content, embed}, channel_id) when is_binary(content) and is_map(embed) and is_integer(channel_id) do
+  def create_message({content, embed}, channel_id) when is_binary(content) and is_map(embed) and (is_integer(channel_id) or is_binary(channel_id)) do
     request(:post, Util.channel_messages(channel_id), %{content: content, embed: embed, tts: false})
     |> handle
   end
